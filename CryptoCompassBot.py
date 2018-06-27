4a15f7c008cba9a7581b00a31a8713a52c32ddbb
ashishkrishan1995 <ashishkrishan1995@gmail.com>
2018-06-28 00:03:14 +0530

    Added log errors

--- CryptoCompassBot.py
+++ CryptoCompassBot.py
@@ -8,15 +8,27 @@ logger = logging.getLogger(__name__)
 
 #All required command handlers
 def start(bot, update):
-    update.message.reply_text("Dear User, \nI'm the CryptoCompass bot. I can help you with the following.\n1. How to Invest?\n2. Current Deal\n3. Survey: Which ICO would you like on CryptoCompass\n4. Submit your ICO.")
+    update.message.reply_text("Dear User, \nI'm the CryptoCompass bot. I can help you with the following.\n\n1. How to Invest?\n2. Current Deal\n3. Survey: Which ICO would you like on CryptoCompass\n4. Submit your ICO.")
 
 def help(bot, update):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
+def invest(bot, update):
+    """How to Invest ?"""
+    update.message.reply_text('')
+
+def submit(bot, update):
+    """Submit your ICO"""
+    update.message.reply_text('')
+
+    
 def echo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
 
 def main():
 
@@ -29,9 +41,14 @@ def main():
     # Different commands
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("invest", invest))
+    dp.add_handler(CommandHandler("submit", submit))
 
     # On non command message, echo the same message
     dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # Log all errors
+    dp.add_error_handler(error)
 
     # Start the bot
     updater.start_polling()
@@ -42,3 +59,4 @@ if __name__ == '__main__':
 
 
 
+     
