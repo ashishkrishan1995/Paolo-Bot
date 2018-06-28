98bfed41c41d3b6dc1ce05832337113f37e893ed
shekhuverma <shekharvrm47@gmail.com>
2018-06-28 15:27:45 +0530

    Added commands

---
+++ rules.py
@@ -0,0 +1,26 @@
+import emoji
+
+st=emoji.emojize("""
+Welcome to the CryptoCompass Telegram
+ 
+We work with a network experts coming from financial and crypto world.
+
+:bar_chart: Serving operational due diligence with more than 150 criteria on the selected projects.
+
+:necktie: Exclusive pre-sale deals and conditions to our members before public sales.
+
+Premium opportunities to participate in private sales for registered members.
+ 
+Follow us on these channels: 
+
+ •Website: https://crypto-compass.io
+ •Telegram group : https://t.me/cryptocompass
+ •Telegram channel : https://t.me/cryptocompassio
+ 
+We advise you to pin our group in  your telegram.
+
+   Attention!!! 
+
+Do not send anything to any other address. Always check our pinned message to verify authenticity and instructions.  Don’t trust anybody telling you anything different than the message stated in our pinned message.
+
+ •  ALWAYS check if the person you are talking to on this chat has the admin mark next to their name.""")
